36640e0a50b298ef815032986ec271b7563423b9
firegloves <lucorset@gmail.com>
2023-02-12 19:48:04 +0100

    initial commit

---
+++ src/main/java/it/firegloves/mempoispringexample/MempoiService.java
@@ -0,0 +1,40 @@
+package it.firegloves.mempoispringexample;
+
+import it.firegloves.mempoi.builder.MempoiBuilder;
+import it.firegloves.mempoi.builder.MempoiSheetBuilder;
+import it.firegloves.mempoi.domain.MempoiReport;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MempoiService {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public MempoiService(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public MempoiReport selectFromTestTable() throws SQLException, ExecutionException, InterruptedException {
+
+        final Connection connection = this.dataSource.getConnection();
+        final PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM test_table");
+
+        final MempoiSheetBuilder mempoiSheetBuilder = MempoiSheetBuilder.aMempoiSheet()
+                .withPrepStmt(preparedStatement);
+
+        return MempoiBuilder.aMemPOI()
+                .addMempoiSheet(mempoiSheetBuilder)
+                .withFile(new File("export_result.xlsx"))
+                .build()
+                .prepareMempoiReport()
+                .get();
+    }
+}
---
+++ src/test/java/it/firegloves/mempoispringexample/MemPoiSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.firegloves.mempoispringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemPoiSpringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
